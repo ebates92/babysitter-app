b197476ea3f698a6ed099d0c8049260c2d65c95a
Evan Bates <ebates92@gmail.com>
2017-12-23 15:35:20 -0600

    fixed selection boxes on calendar to update when you select it again

--- babysitter/scripts/calendar.js
+++ babysitter/scripts/calendar.js
@@ -1,5 +1,10 @@
 
-$('.day-box').on("click", function () {
+$('.day-box').on("click", function (event) {
     console.log(event.target)
     $(this).toggleClass('selected');
+    if (event.target.innerHTML === '') {
+        event.target.innerHTML = "&check;";
+    } else {
+        event.target.innerHTML = "";
+    }
 });
