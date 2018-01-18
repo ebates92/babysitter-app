3a9510f1b64e2abd6380f649bfe37b565e322f8e
Alexander Cleoni <alexander.cleoni@gmail.com>
2018-01-18 15:16:21 -0500

    added submit button to from to save user info

--- public/scripts/slidingform.js
+++ public/scripts/slidingform.js
@@ -26,6 +26,29 @@ function showSlides(n){
 	dots[slideIndex - 1].className += " active";
 }
 
+function likesPets () {
+    $(".likes-pets").on('click', function (event) {
+        $("#pets").removeClass('hidden');
+    });
+};
+
+function doesNotLikePets () {
+    $(".does-not-like-pets").on('click', function (event) {
+        $("#pets").addClass('hidden');
+        // $("input[name=dog]").checked = false;
+        // NEED TO FIX SINCE I CAN'T GET THE CHECKBOX ON THE DOG/CAT TO BE REMOVED
+        $(".dog").checked = false;
+        $(".cat").checked = false;
+        console.log($(".dog"));
+    });
+};
+
+
+likesPets();
+doesNotLikePets();
+
+
+
 
 
 
