d06916cb5db7ab9b92dde8fccfcfee41e2e6e410
Alexander Cleoni <alexander.cleoni@gmail.com>
2018-01-19 10:41:48 -0500

    Adds css transitions to form

--- public/scripts/slidingform.js
+++ public/scripts/slidingform.js
@@ -17,13 +17,13 @@ function showSlides(n){
 	if (n > slides.length) {slideIndex = 1}
 	if (n < 1) {slideIndex = slides.length}
 	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
+		slides[i].classList.remove("active");
 	}
 	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "");	
+	    dots[i].classList.remove("active");	
 	}
-	slides[slideIndex - 1].style.display = "block";
-	dots[slideIndex - 1].className += " active";
+	slides[slideIndex - 1].classList.add("active");
+	dots[slideIndex - 1].classList.add("active");
 }
 
 function likesPets () {
