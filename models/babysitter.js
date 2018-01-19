48a8283c1eefdfe27009ab9a6f43c8c995b7ba60
Evan Bates <ebates92@gmail.com>
2018-01-18 20:21:23 -0500

    posting to database works. Needs edits for validation

--- models/babysitter.js
+++ models/babysitter.js
@@ -5,23 +5,23 @@ const Babysitter = connection.define('babysitter', {
     emailaddress: {
         type: Sequelize.STRING,
         unique: true,
-        allowNull: false,
-        validate: {
-            is: {
-                args: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                msg: 'Please enter a valid email address.'
-            }
-        }
+        // allowNull: false,
+        // validate: {
+        //     is: {
+        //         args: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        //         msg: 'Please enter a valid email address.'
+        //     }
+        // }
     },
     password: {
         type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            is: {
-                args: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/,
-                msg: 'Passwords must contain a combination of 6 characters, capital letters, lowercase letters and special characters.'
-            }
-        }
+        // allowNull: false,
+        // validate: {
+        //     is: {
+        //         args: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/,
+        //         msg: 'Passwords must contain a combination of 6 characters, capital letters, lowercase letters and special characters.'
+        //     }
+        // }
     },
     firstname: {
         type: Sequelize.STRING,
@@ -42,32 +42,32 @@ const Babysitter = connection.define('babysitter', {
     },
     zipcode: {
         type: Sequelize.INTEGER,
-        allowNull: false,
+        // allowNull: false,
     },
     image: {
         type: Sequelize.STRING,
     },
     birthyear: {
         type: Sequelize.INTEGER,
-        allowNull: false,
+        // allowNull: false,
     },
     birthmonth: {
         type: Sequelize.INTEGER,
-        allowNull: false,
+        // allowNull: false,
     },
     birthdate: {
         type: Sequelize.INTEGER,
-        allowNull: false,
+        // allowNull: false,
     },
     gender: {
         type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            is: {
-                args: /^male$|^female$/,
-                msg: 'The value for gender must be either male or female.'
-            }
-        }
+        // allowNull: false,
+        // validate: {
+        //     is: {
+        //         args: /^male$|^female$/,
+        //         msg: 'The value for gender must be either male or female.'
+        //     }
+        // }
     },
     maxchildren: {
         type: Sequelize.INTEGER,
@@ -77,23 +77,23 @@ const Babysitter = connection.define('babysitter', {
     },
     hourlyrate: {
         type: Sequelize.INTEGER,
-        allowNull: false,
+        // allowNull: false,
     },
     mileswilling: {
         type: Sequelize.INTEGER,
-        allowNull: false,
+        // allowNull: false,
     },
     agegroup: {
         type: Sequelize.STRING,
     },
     summary: {
         type: Sequelize.TEXT,
-        validate: {
-            len:{
-                args: [20,150],
-                msg: 'Please enter between 20 - 150 characters.'
-            }
-        }
+        // validate: {
+            // len:{
+            //     args: [20,150],
+            //     msg: 'Please enter between 20 - 150 characters.'
+            // }
+        // }
     },
     education: {
         type: Sequelize.STRING,
@@ -107,53 +107,53 @@ const Babysitter = connection.define('babysitter', {
     },
     transportation: {
         type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            is: {
+        // allowNull: false,
+        // validate: {
+        //     is: {
                 // arg: /^yes$|^no$/,
                 // msg: 'The value for transportation must be yes or no.'
-            }
-        }
+        //     }
+        // }
     },
     smoke: {
         type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            is: {
+        // allowNull: false,
+        // validate: {
+        //     is: {
                 // arg: /^yes$|^no$/,
                 // msg: 'The value for smoking must be yes or no.'
-            }
-        }
+        //     }
+        // }
     },
     pets: {
         type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            is: {
+        // allowNull: false,
+        // validate: {
+        //     is: {
                 // arg: /^yes$|^no$/,
                 // msg: 'The value for liking pets must be yes or no.'
-            }
-        }
+        //     }
+        // }
     },
     cats: {
         type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            is: {
+        // allowNull: false,
+        // validate: {
+        //     is: {
                 // arg: /^yes$|^no$/,
                 // msg: 'The value for liking cats must be yes or no.'
-            }
-        }
+        //     }
+        // }
     },
     dogs: {
         type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            is: {
+        // allowNull: false,
+        // validate: {
+        //     is: {
                 // arg: /^yes$|^no$/,
                 // msg: 'The value for liking dogs must be yes or no.'
-            }
-        }
+        //     }
+        // }
     }
 });
 
