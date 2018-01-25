83bfca57ca715abe3250cd5e265312c2cbfbc659
Chris Rithmyxay <crithmyxay@gmail.com>
2018-01-25 13:33:08 -0500

    fixed routing error

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const connection = new Sequelize('BABYSITTER_APP', 'evanbates','', {
+const connection = new Sequelize('BABYSITTER_APP', 'christopher.rithmyxay','', {
     host:'localhost',
     dialect: 'postgres'
 });
